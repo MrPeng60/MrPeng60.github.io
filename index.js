62097401a5e4fa491c8b12eb05389aa6fe93e690
MrPeng60 <139284832+MrPeng60@users.noreply.github.com>
2023-07-12 01:21:38 +0100

    no sum

---
+++ index.js
@@ -0,0 +1,46 @@
+const iframe = document.querySelector("iframe");
+iframe.setAttribute("src", src());
+
+function src(){
+
+	
+
+	/*
+	100k https://cdnp.kink.com/imagedb/104792/trailer/104792_trailer_high.mp4
+	30k https://cdnp.kink.com/imagedb/37843/v/h/320/hires/37843_7.mp4
+	*/
+}
+
+function nrand(){
+
+}
+function next(){
+
+}
+function prev(){
+
+}
+
+function volup(){
+
+}
+function voldown(){
+
+}
+
+function pause(){
+
+}
+function forw(){
+
+}
+function back(){
+
+}
+
+function faster(){
+
+}
+function slower(){
+
+}

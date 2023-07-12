fc66bb712e5d4f526343793a04ba58390db2b78b
MrPeng60 <139284832+MrPeng60@users.noreply.github.com>
2023-07-12 02:19:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const iframe = document.querySelector("iframe");
-iframe.setAttribute("src", src());
-
-function src(){
-
-	return "https://cdnp.kink.com/imagedb/44284/v/h/320/hires/44284_7.mp4";
-
-	/*
-	100k https://cdnp.kink.com/imagedb/104792/trailer/104792_trailer_high.mp4
-	30k https://cdnp.kink.com/imagedb/37843/v/h/320/hires/37843_7.mp4
-	*/
-}
+const iframe = document.querySelector('iframe');
+iframe.addEventListener('load', () => {
+  const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+  const video = iframeDocument.querySelector('video');
+  video.addEventListener('ended', () => {
+    console.log('Video ended');
+		next();
+  });
+	video.addEventListener('error', () => {
+    console.log('Video threw an error');
+		nrand();
+  });
+});
 
 function nrand(){
-
+	iframe.setAttribute("src", "https://cdnp.kink.com/imagedb/104792/trailer/104792_trailer_high.mp4";
 }
 function next(){
-
+	iframe.setAttribute("src", "https://cdnp.kink.com/imagedb/104792/trailer/104792_trailer_high.mp4";
 }
 function prev(){
-
+	iframe.setAttribute("src", "the source";
 }
 
 function volup(){
@@ -44,3 +45,8 @@ function faster(){
 function slower(){
 
 }
+
+/*
+	100k https://cdnp.kink.com/imagedb/104792/trailer/104792_trailer_high.mp4
+	30k https://cdnp.kink.com/imagedb/37843/v/h/320/hires/37843_7.mp4
+	*/
